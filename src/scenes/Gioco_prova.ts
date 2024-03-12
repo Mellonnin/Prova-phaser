e42bf758ed02f2558eb69c799ada318ea6cc0983
Mellonnin <falciano.marco.25@gmail.com>
2024-03-12 22:57:58 +0100

    rotto

--- src/scenes/Gioco_prova.ts
+++ src/scenes/Gioco_prova.ts
@@ -7,7 +7,7 @@ import TextureKeys from "../consts/TextureKeys";
 
 export default class Gioco_prova extends Phaser.Scene {
   Giocatore: Phaser.Physics.Arcade.Sprite;
-  Piattaforma: Phaser.Physics.Arcade.Sprite;
+  //Piattaforma: Phaser.Physics.Arcade.Sprite;
   Punti: Phaser.Physics.Arcade.StaticGroup;
   Camera: Phaser.Cameras.Scene2D.Camera;
   //Cursore: Phaser.Types.Input.Keyboard.CursorKeys;
@@ -31,15 +31,21 @@ export default class Gioco_prova extends Phaser.Scene {
     this.A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.SPACE = this.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.SPACE
-    );
+    this.SPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-    //this.Camera = this.cameras.main;
-    //this.Camera.setBounds(0, 0, this.game.canvas.width, this.game.canvas.height, true);
+
+    this.Camera = this.cameras.main;
+    this.Camera.setBounds(
+      0,
+      0,
+      this.game.canvas.width,
+      this.game.canvas.height,
+      true
+    );
     //this.Cursore = this.input.keyboard.createCursorKeys();
   }
   create() {
+<<<<<<< HEAD
     this.platforms = this.physics.add.staticGroup()
     const firstPlatform = this.platforms.create(
       gameSettings.gameWidth/2,
@@ -48,6 +54,23 @@ export default class Gioco_prova extends Phaser.Scene {
     ).setScale(0.1);
     const firstPlatformBody = firstPlatform.body;
     firstPlatformBody.updateFromGameObject();
+=======
+
+    this.Piattaforme = this.physics.add.group({
+      runChildUpdate: true,
+      maxSize: 100,
+    });
+
+
+    const spawnImage = () => {
+      let _image = this.add
+        .image(gameSettings.gameWidth * 0.5 * Math.random(), 0, TextureKeys.Piattaforma)
+        .setOrigin(0, 2)
+        .setScale(0.2)
+        .setDepth(1);
+      this.Piattaforme.add(_image);
+    };
+>>>>>>> parent of 02071a76 (movimento)
 
     this.x = (gameSettings.gameWidth/3);
     this.y = 100
@@ -61,8 +84,11 @@ export default class Gioco_prova extends Phaser.Scene {
       
       platform.setScale(0.1)
 
+<<<<<<< HEAD
       this.platforms.add(platform)
       this.y += 200;
+=======
+>>>>>>> parent of 02071a76 (movimento)
 
       const body = platform.body as Physics.Arcade.StaticBody
       body.updateFromGameObject()
@@ -71,58 +97,64 @@ export default class Gioco_prova extends Phaser.Scene {
       this.Giocatore = this.physics.add
       .sprite(
         gameSettings.gameWidth * 0.5,
-        gameSettings.gameHeight,
+        gameSettings.gameHeight * 1,
         TextureKeys.Giocatore
       )
       .setBounce(0, 0)
       .setCollideWorldBounds(true)
-      .setDrag(0.1, 0.1)
+      .setDrag(0, 0)
       .setMaxVelocity(900, 900)
       .setGravity(0, 450)
-      .setScale(0.2)
-      .setFriction(0.5, 0.5);
+      .setScale(0.2);
 
     this.physics.add.collider(
       this.Giocatore,
+<<<<<<< HEAD
       this.platforms
     );
 
     //this.Camera.startFollow(this.Giocatore, true, 0.05, 0.05);
     this.physics.add.collider(this.platforms, this.Giocatore);
     this.physics.add.overlap(this.Giocatore, this.platforms);
+=======
+      this.Piattaforme
+    );
+
+    this.Camera.startFollow(
+      this.Giocatore,
+      true,
+      0.05,
+      0.05
+    );
+
+    //this.physics.add.collider(this.Piattaforme, this.Camera)
+    //this.physics.add.overlap(this.Giocatore, this.Piattaforme, da capire come fare detectare ogni lato () => { this.Piattaforma.setY(+100)});
+>>>>>>> parent of 02071a76 (movimento)
   }
 
   update(time: number, delta: number): void {
-    this.physics.add.collider(this.Giocatore, this.Piattaforma);
-
-    this.physics.add.overlap(this.Giocatore, this.Piattaforma, () => {
-      if (this.Giocatore.body.touching.up) {
-        this.Giocatore.setVelocityY(-5000);
-      } else if (this.Giocatore.body.touching.down) {
-        this.Giocatore.setVelocityY(5000);
-      } else if (this.Giocatore.body.touching.left) {
-        this.Giocatore.setVelocityX(5000);
-      } else if (this.Giocatore.body.touching.right) {
-        this.Giocatore.setVelocityX(-5000);
-      }
-    });
-
-    const speed = 500;
-
-    this.Giocatore.setVelocity(0);
-
     if (this.A.isDown) {
-      this.Giocatore.setVelocityX(-speed);
+      this.Giocatore.x -= 10;
     } else if (this.D.isDown) {
-      this.Giocatore.setVelocityX(speed);
+      this.Giocatore.x += 10;
     }
     if (this.S.isDown) {
+<<<<<<< HEAD
       this.Giocatore.setAccelerationY(speed);
       //this.cameras.main.pan(0, 5, 1000, "Sine.easeInOut", true);
     } else if (this.SPACE.isDown) {
       this.Giocatore.setAccelerationY(-speed);
       //this.cameras.main.pan(0, 10, 1000, "Sine.easeInOut", true);
     }
+=======
+      this.Giocatore.y += 10;
+      //this.cameras.main.pan(0, 5, 1000, "Sine.easeInOut", true);
+    } else if (this.SPACE.isDown) {
+      this.Giocatore.y -= 10;
+      //this.cameras.main.pan(0, 10, 1000, "Sine.easeInOut", true);
+    }
+    // da capire come prendere la posizione
+>>>>>>> parent of 02071a76 (movimento)
     
   }
 }
